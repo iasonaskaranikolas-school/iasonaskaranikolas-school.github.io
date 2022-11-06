332a5d7c18a659db4dbe1c3c393ced4795954b15
iasonaskaranikolas <117376388+iasonaskaranikolas@users.noreply.github.com>
2022-11-07 00:18:04 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+const loadJS = (url, cb) => {
+    try {
+        const scriptTag = document.createElement("script");
 
+        scriptTag.src = url;
+        scriptTag.onload = cb;
+        scriptTag.onreadystatechange = cb;
+
+        document.getElementsByTagName("head")[0].appendChild(scriptTag);
+    } catch { }
+};
+
+window.onload = async () => {
+    if ((window.Fingerprinter === null || window.Fingerprinter === undefined) || (window.UAParser === null || window.UAParser === undefined) || (window.CryptoJS === null || window.CryptoJS === undefined)) throw new Error("Fingerprinter could not be loaded!");
+    const client = new window.Fingerprinter();
+}
