10b58695e4eaa5605f6fd9d9752ba565132c5c0c
iasonaskaranikolas-school <117376388+iasonaskaranikolas-school@users.noreply.github.com>
2022-11-25 09:10:25 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const pages = {
+    "home": "/",
+    "school": "/views/school.html",
+    "about": "/views/about.html"
+}
+
+try {
+    const topnav = document.getElementById();
+} catch (e) {
+    console.log(e);
+}
