6a7ef3ec0e3cf37286e527a765bed9497464bf37
iasonaskaranikolas-school <117376388+iasonaskaranikolas-school@users.noreply.github.com>
2022-11-30 19:09:12 +0200

    update

--- js/schedule_fetcher.js
+++ js/schedule_fetcher.js
@@ -6,6 +6,12 @@ try {
 	isBrowser = false;
 }
 
+let url = "http://2epal-chalandr.att.sch.gr/index_htm_files/2022%20Programma%20ana%20Tmima%2011%2007.html";
+
+if (isBrowser === true) {
+	url = `https://cors-everywhere.tk/get?q=${url}`;
+}
+
 const grabSchedule = async (elem) => {
 	const req = await fetch("http://2epal-chalandr.att.sch.gr/index_htm_files/2022%20Programma%20ana%20Tmima%2011%2007.html");
 	if (req !== null && req !== undefined) {
