d864b3c9d3777f411a6f6735521281359e0e353d
iasonaskaranikolas-school <117376388+iasonaskaranikolas-school@users.noreply.github.com>
2022-12-02 18:31:33 +0200

    update

--- js/main.js
+++ js/main.js
@@ -31,8 +31,11 @@ const loadTopNav = () => {
                 }
             }
             const copyright = document.createElement("a");
-            copyright.innerText = `© Copyright 2022 - 2022 Jasonkaranik`;
+            copyright.innerText = `© Copyright ${new Date().getFullYear()} Jasonkaranik`;
             copyright.setAttribute("style", "float: right; text-align: right;");
+            copyright.setAttribute("href", "https://en.wikipedia.org/wiki/Copyright");
+            copyright.setAttribute("target", "_blank");
+            copyright.setAttribute("rel", "noreferrer");
             topnav.appendChild(copyright);
         }
     } catch (e) {
