539ebe2276ad2098513fd58937105f4c00388bd4
iasonaskaranikolas-school <117376388+iasonaskaranikolas-school@users.noreply.github.com>
2023-05-20 23:31:20 +0300

    update

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ const queryMetroDepartureTime = async () => {
     const req = await fetch("https://eval.jasonkaranik.tk/", {
         method: "POST",
         headers: {
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"
+            "content-type": "application/json"
         },
         body: JSON.stringify({
             "key": "hi",
